4007e4a19053be6dc35e2ca462854bb21f0da3a0
unknown <aidahosa@kazzalogy.com>
2019-11-24 11:23:21 -0500

    random id

--- src/app/existing-key/existing-key.component.ts
+++ src/app/existing-key/existing-key.component.ts
@@ -25,7 +25,6 @@ export class ExistingKeyComponent implements OnInit {
 
   ngOnInit() {
     this.isDataEncrypted = false;
-    this.keyMan.currentKey = '4928520184HAKIA242';
     this.isViewingMyData = true;
     this.myData = [];
 
--- src/app/key-manager.service.ts
+++ src/app/key-manager.service.ts
@@ -6,10 +6,17 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class KeyManagerService {
   currentKey: string;
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.generateNewID();
+  }
 
   generateNewID() {
-    this.currentKey = '6482749KDJF293JSA';
+    const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let array = new Uint8Array(40);
+    window.crypto.getRandomValues(array);
+    array = array.map(x => validChars.charCodeAt(x % validChars.length));
+    const randomState = String.fromCharCode.apply(null, array);
+    this.currentKey = randomState;
 
   }
 
